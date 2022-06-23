bcf59ef8bf25c3ff10448ac6b23e72d80e61750c
edmariooliver <edmariooliver@outlook.com>
2022-06-23 15:45:20 -0300

    Criando testes dos services

--- src/test/java/com/pluc/pluc/services/LinkServiceTests.java
+++ src/test/java/com/pluc/pluc/services/LinkServiceTests.java
@@ -4,16 +4,14 @@ import java.time.Instant;
 
 import javax.transaction.Transactional;
 
-import org.apache.logging.log4j.message.ReusableMessage;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.pluc.pluc.dto.LinkDTO;
 import com.pluc.pluc.entities.Link;
-import com.pluc.pluc.services.LinkService;
+import com.pluc.pluc.services.exceptions.ResourceEntityNotFoundException;
 
 @SpringBootTest
 @Transactional
@@ -32,6 +30,23 @@ public class LinkServiceTests {
 
     @Test
     public void deleteShouldTrowsExceptionWhenIdDoesNotExists() {
-        
+        Long invalidId = 1000L;
+        Assertions.assertThrows(ResourceEntityNotFoundException.class, ()->{
+            service.delete(invalidId);
+        });
+    }
+
+    @Test
+    public void findByShortenedShouldReturnsResourceEntityNotFoundExceptionWhenDoesNotShortenedExists() {
+        String shortenedInvalid = "aaaaa";
+        Assertions.assertThrows(ResourceEntityNotFoundException.class, ()->{
+            service.findByShortened(shortenedInvalid);
+        });
+    }
+    
+    @Test
+    public void getByIdShouldReturnDtoWhenIdExists() {
+        Long idExists = 1L;
+        Assertions.assertInstanceOf(LinkDTO.class, service.getById(idExists));
     }
 }
